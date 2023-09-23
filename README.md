cbf247e4a7b60ea18c6cc1a8feaed5514545fc79
2413 莊育誠 lionchuang <lionchuang@pershing.com.tw>
2023-09-23 17:32:53 +0800

    0.7： 留言納入紀錄條件

--- README.md
+++ README.md
@@ -8,7 +8,7 @@
 [油猴](https://greasyfork.org/zh-TW/scripts/475916-%E5%B7%B4%E5%8F%8B%E6%9A%B1%E7%A8%B1%E7%B4%80%E9%8C%84)  
 [Github](https://github.com/Mr-Smilin/bahamut-name-record)
 
-當前版本： 0.6
+當前版本： 0.7
 
 <details> <summary>版本紀錄</summary>  
   <br>  
@@ -19,6 +19,7 @@
 - 0.4： 嘗試改用 localforage 存放資料
 - 0.5： 嘗試改用 indexedDB 存放資料
 - 0.6： 取消序列化存放
+- 0.7： 留言納入紀錄條件
   
 </details>
   
@@ -28,12 +29,12 @@
 
 - [x] 發文者名稱紀錄、歷史查詢(如果有存)
 - [x] localStorage 棄用，改成 indexedDB
+- [x] 巴友留言納入記錄條件
 
 <br>
 
 預計開發
 
 - [ ] 改名觸發高亮提醒
-- [ ] 巴友留言納入記錄條件
 - [ ] 查詢功能
 - [ ] 備註替換暱稱功能
--- bahamut-name-record.js
+++ bahamut-name-record.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         巴友暱稱紀錄
 // @namespace    https://forum.gamer.com.tw
-// @version      0.6
+// @version      0.7
 // @description  發文者暱稱紀錄
 // @author       You
 // @match        https://forum.gamer.com.tw/C.php*
@@ -116,8 +116,10 @@
 	function clickButton() {
 		const button = document.createElement("button");
 		button.type = "button";
-		button.className = "floor tippy-gpbp";
+		button.className = "usertitle";
 		button.setAttribute("isshow", "false");
+		button.style.borderWidth = "0px";
+		button.style.padding = "1px 6px";
 		button.onclick = function () {
 			showMessage(this);
 		};
@@ -137,6 +139,37 @@
 
 		return div;
 	};
+
+	const replyDiv = (localStor, contentUser) => {
+		const div = document.createElement("div");
+		div.className = "name-list-reply-div";
+		div.style.position = "relative";
+		div.style.display = "inline";
+
+		const names = nameList(localStor);
+		setHoverShow(contentUser, names);
+
+		div.appendChild(names);
+
+		return div;
+	};
+	//#endregion
+
+	//#region 一些 DOM 模組化的效果
+	function setHoverShow(triggerElement, targetElement) {
+		// 初始隱藏 target 元素
+		targetElement.style.display = "none";
+
+		// 當 trigger 元素被滑過時，顯示 target 元素
+		triggerElement.addEventListener("mouseover", function () {
+			targetElement.style.display = "block";
+		});
+
+		// 當滑鼠離開 trigger 元素時，隱藏 target 元素
+		triggerElement.addEventListener("mouseout", function () {
+			targetElement.style.display = "none";
+		});
+	}
 	//#endregion
 
 	//#region 資料管理 api
@@ -191,9 +224,17 @@
 	}
 	//#endregion
 
-	// dom 渲染
+	//#region dom 渲染
 	async function render() {
-		const dom = document.querySelectorAll(".c-post__header__author");
+		const postDom = document.querySelectorAll(".c-post__header__author");
+		const replyDom = document.querySelectorAll(".reply-content");
+		await renderPostHeader(postDom);
+		await renderReplyContent(replyDom);
+		listenerAllMoreReplyButton();
+	}
+
+	// 渲染發文者
+	async function renderPostHeader(dom) {
 		for (let d of dom) {
 			const userid = d.querySelector(".userid").textContent;
 			const username = d.querySelector(".username").textContent.trim();
@@ -201,6 +242,50 @@
 			d.appendChild(mainDiv(localStor[userid]));
 		}
 	}
+
+	// 渲染回文者
+	async function renderReplyContent(dom) {
+		for (let d of dom) {
+			const contentUser = d.querySelector(".reply-content__user");
+			// const contentFooter = d.querySelector(".reply-content__footer");
+			const match = contentUser.href.match(/home.gamer.com.tw\/([^\/]+)/);
+			const userid = match ? match[1] : null;
+			if (!userid) continue;
+			const username = contentUser.textContent.trim();
+			const localStor = await searchUsername(userid, username);
+			contentUser.appendChild(replyDiv(localStor[userid], contentUser));
+			d.style.overflow = "unset";
+			d.setAttribute("data-modified", "true");
+		}
+	}
+	//#endregion
+
+	//#region 按鍵監聽 & DOM 渲染
+	function listenerAllMoreReplyButton() {
+		let timeoutID = null;
+		const moreButtons = document.querySelectorAll(".more-reply");
+		const hideButtons = document.querySelectorAll(".hide-reply");
+		for (let button of moreButtons) {
+			button.addEventListener("click", function () {
+				clearTimeout(timeoutID);
+				timeoutID = setTimeout(() => {
+					// 獲取所有未被標記過的元素
+					const dom = document.querySelectorAll(
+						".reply-content:not([data-modified])"
+					);
+					renderReplyContent(dom);
+				}, 3000);
+			});
+		}
+		for (let button of hideButtons) {
+			button.addEventListener("click", function () {
+				clearTimeout(timeoutID);
+				// 折疊後 data-modified 還在，功能卻要重設，太麻煩了
+				// 太麻煩了，先清除 timeout 就好...
+			});
+		}
+	}
+	//#endregion
 
 	// 歷史紀錄 click
 	const showMessage = function showMessage(element) {
